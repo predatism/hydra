5e3dfe50b949b51c87369b9387f849fb3d22eb7a
predatism <96994785+predatism@users.noreply.github.com>
2022-01-03 00:59:02 +0500

    Add files via upload

---
+++ Md5Checker/Md5Checker/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+string CreateMD5(string myText)
+{
+    var hash = MD5.Create().ComputeHash(Encoding.ASCII.GetBytes(myText ?? ""));
+    return string.Join("", Enumerable.Range(0, hash.Length).Select(i => hash[i].ToString("x2")));
+}
+
+Console.Write("Enter First MD5 -> ");
+var FirstMD5 = Console.ReadLine();
+
+Console.Write("Enter Round -> ");
+var Round = Console.ReadLine();
+
+long MaxNumber = 999999999999999999;
+
+for (int n = 0; n < MaxNumber; n++)
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        var GeneratedNumber = n.ToString("000000000000000000");
+
+        string FinalString = $"{Round}.{i}.{GeneratedNumber}";
+
+        var FinalMD5 = CreateMD5(FinalString);
+
+        Console.WriteLine(FinalString);
+
+        if (FirstMD5 == FinalMD5)
+        {
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
